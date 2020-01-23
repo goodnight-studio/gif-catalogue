978b932f71443d2217482a8e19e2d3bc1fb3b268
Geof Crowl <rectangular@gmail.com>
2020-01-22 17:15:06 -0700

    Initial Commit

---
+++ GIF Catalogue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GIF Catalogue
+//
+//  Created by Geof Crowl on 1/22/20.
+//  Copyright © 2020 Geof Crowl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
