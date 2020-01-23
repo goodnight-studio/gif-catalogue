b540442b480e54a524ba625063f91e8a0e925179
Geof Crowl <rectangular@gmail.com>
2020-01-22 17:25:18 -0700

    Remove storyboard

--- GIF Catalogue/RootViewController.swift
+++ GIF Catalogue/RootViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func loadView() {
+        print("here")
+        
+        view = UIView()
     }
 
 
